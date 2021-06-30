62cf9cabb5513c7432173eb48cf8842af94b1603
MimiBG <michel-bg@hotmail.com>
2021-06-30 20:32:35 +0200

    GLA Model and Repository's for Generic and migrations

--- app/Services/LAPUpdater.php
+++ app/Services/LAPUpdater.php
@@ -7,11 +7,12 @@ namespace App\Services;
 use App\Category;
 use App\Chain;
 use App\ChainManager;
-use App\Difficulty;
-use App\LearningActivityProducing;
+use App\Column;
+use App\column_data;
+use App\fieldtype;
 use App\ResourceMaterial;
-use App\Status;
 use Carbon\Carbon;
+use App\Genericlearningactivity;
 
 class LAPUpdater
 {
@@ -25,55 +26,109 @@ class LAPUpdater
         $this->chainManager = $chainManager;
     }
 
-    public function update(LearningActivityProducing $learningActivityProducing, array $data): bool
+    public function update(Genericlearningactivity $genericlearningactivity, $data): bool
     {
-        $learningActivityProducing->date = Carbon::parse($data['datum'])->format('Y-m-d');
-        $learningActivityProducing->description = $data['omschrijving'];
-        $learningActivityProducing->duration = $data['aantaluren'] !== 'x' ? $data['aantaluren'] : round(
-            ((int) $data['aantaluren_custom']) / 60,
-            2
-        );
+        //choosing fieldtypes from database
+        $radiobutton = Fieldtype::where("fieldtype","radiobutton")->first();
+        $text = Fieldtype::where("fieldtype","text")->first();
+        $datePicker = Fieldtype::where("fieldtype","date")->first();
+        $button = Fieldtype::where("fieldtype","date")->first();
 
-        switch ($data['resource']) {
-            case 'persoon':
-                $learningActivityProducing->resourcePerson()->associate($data['personsource']);
-                $learningActivityProducing->resourceMaterial()->dissociate();
-                $learningActivityProducing->res_material_detail = null;
-                break;
-            case 'internet':
-                $learningActivityProducing->resourceMaterial()->associate((new ResourceMaterial())->find(1));
-                $learningActivityProducing->res_material_detail = $data['internetsource'];
-                $learningActivityProducing->resourcePerson()->dissociate();
-                break;
-            case 'boek':
-                $learningActivityProducing->resourceMaterial()->associate((new ResourceMaterial())->find(2));
-                $learningActivityProducing->res_material_detail = $data['booksource'];
-                $learningActivityProducing->resourcePerson()->dissociate();
-                break;
-            case 'alleen':
-                $learningActivityProducing->resourcePerson()->dissociate();
-                $learningActivityProducing->resourceMaterial()->dissociate();
-                $learningActivityProducing->res_material_detail = null;
-                break;
+        if (isset($data['datum'])) {
+            $column = $this->createColumn("date", null, Carbon::parse($data['datum'])->format('Y-m-d'), $datePicker, "date");
+            $genericlearningactivity->column()->associate($column);
+        }
+        if (isset($data['omschrijving'])) {
+            $column = $this->createColumn("description", null, $data['omschrijving'], $text, "string");
+            $genericlearningactivity->column()->associate($column);
+        }
+        if (isset($data['aantaluren']) || isset($data['aantaluren_custom'])) {
+            $description = $data['aantaluren'] !== 'x' ? $data['aantaluren'] : round(
+                ((int) $data['aantaluren_custom']) / 60, 2);
+            $columnOptions = "[0.25,0.50,0.75]";
+            $column = $this->createColumn("duration", $columnOptions, $description, $button, "float");
+            $genericlearningactivity->column()->associate($column);
         }
 
-        $learningActivityProducing->category()->associate((new Category())->find($data['category_id']));
-        $learningActivityProducing->difficulty()->associate((new Difficulty())->find($data['moeilijkheid']));
-        $learningActivityProducing->status()->associate((new Status())->find($data['status']));
-
-        $chainId = $data['chain_id'] ?? null;
-
-        if ($chainId !== null) {
-            if (((int) $chainId) === -1) {
-                $learningActivityProducing->chain_id = null;
-            } elseif (((int) $chainId) !== -1) {
-                $chain = (new Chain())->find($chainId);
-                if ($chain->status !== Chain::STATUS_FINISHED) {
-                    $this->chainManager->attachActivity($learningActivityProducing, $chain);
-                }
+        if (isset($data['resource'])) {
+            switch ($data['resource']) {
+                case 'persoon':
+                    $genericlearningactivity->resourcePerson()->associate($data['personsource']);
+                    $genericlearningactivity->resourceMaterial()->dissociate();
+                    $columnOptions = "['persoon','internet','boek', 'alleen']";
+                    $column = $this->createColumn("res_material_detail", $columnOptions, null, $button, "string");
+                    $genericlearningactivity->column()->associate($column);
+                    break;
+                case 'internet':
+                    $genericlearningactivity->resourceMaterial()->associate((new ResourceMaterial())->find(1));
+                    $columnOptions = "['persoon','internet','boek', 'alleen']";
+                    $column = $this->createColumn("res_material_detail", $columnOptions, $data['internetsource'], $button, "string");
+                    $genericlearningactivity->column()->associate($column);
+                    $genericlearningactivity->resourcePerson()->dissociate();
+                    break;
+                case 'boek':
+                    $genericlearningactivity->resourceMaterial()->associate((new ResourceMaterial())->find(2));
+                    $columnOptions = "['persoon','internet','boek', 'alleen']";
+                    $column = $this->createColumn("res_material_detail", $columnOptions, $data['booksource'], $button, "string");
+                    $genericlearningactivity->column()->associate($column);
+                    $genericlearningactivity->resourcePerson()->dissociate();
+                    break;
+                case 'alleen':
+                    $genericlearningactivity->resourcePerson()->dissociate();
+                    $genericlearningactivity->resourceMaterial()->dissociate();
+                    $columnOptions = "['persoon','internet','boek', 'alleen']";
+                    $column = $this->createColumn("res_material_detail", $columnOptions, null, $button, "string");
+                    $genericlearningactivity->column()->associate($column);
+                    break;
             }
         }
 
-        return $learningActivityProducing->save();
+        if (isset($data["category_id"])) {
+            $genericlearningactivity->category()->associate((new Category())->find($data['category_id']));
+        }
+
+        if (isset($data["moeilijkheid"])) {
+            $columnOptions = "['makkelijk','gemiddeld','moeilijk']";
+            $column = $this->createColumn("difficulty", $columnOptions, $data['moeilijkheid'], $button, "string");
+            $genericlearningactivity->column()->associate($column);
+        }
+
+        if (isset($data["status"])) {
+            $columnOptions = "['afgerond','mee bezig','overgedragen']";
+            $column = $this->createColumn("status", $columnOptions, $data['status'], $button, "string");
+            $genericlearningactivity->column()->associate($column);
+        }
+
+//        Based on model LAA & LAP instead of GLA
+//        $chainId = $data['chain_id'] ?? null;
+//
+//        if ($chainId !== null) {
+//            if (((int) $chainId) === -1) {
+//                $genericlearningactivity->chain_id = null;
+//            } elseif (((int) $chainId) !== -1) {
+//                $chain = (new Chain())->find($chainId);
+//                if ($chain->status !== Chain::STATUS_FINISHED) {
+//                    $this->chainManager->attachActivity($genericlearningactivity, $chain);
+//                }
+//            }
+//        }
+
+        return $genericlearningactivity->save();
+    }
+
+    private function createColumn($name, $columnOptions, $data, $fieldType, $dataType) {
+        $column = new Column;
+        $column->name = $name;
+        $column->columnOptions = $columnOptions;
+        $column->fieldType()->associate($fieldType);
+        $column->save();
+
+        $columnData = new column_data();
+        $columnData->column()->associate($column);
+        $columnData->data_as_string = $data;
+        $columnData->dataType = $dataType;
+        $columnData->save();
+
+        return $column;
     }
 }
